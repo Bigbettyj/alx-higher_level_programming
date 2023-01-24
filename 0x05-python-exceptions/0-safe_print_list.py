daab4e3de440c49313d9997c232cc878f77dd399
Bigbettyj <bigbettyj7@gmail.com>
2023-01-24 01:06:50 -0800

    safe printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """prints x elements of a list
+    my_list: the list to print from
+    x: the number of elements to print.
+    Return: the real number of elements printed
+    """
+    n = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            n += 1
+        except IndexError:
+            break
+        print("")
+        return (n)
